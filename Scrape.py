e5a594904a2e37095bbb8814e9eb8e1975febff9
ConanKapoor <kapoor.shivam88@gmail.com>
2017-12-14 19:20:36 +0530

    Exceptions fixed - 1 feature added - Top 10 Reviewer

--- Scrape.py
+++ Scrape.py
@@ -10,220 +10,257 @@ Github : https://github.com/ConanKapoor/Amazon-Review-Scraper
 
 # Importing Essesnsials.
 from bs4 import BeautifulSoup
-import xlsxwriter
 from pyfiglet import Figlet
+import xlsxwriter
 import urllib.request
-import os,time
+import time
 import sys
+import csv
+import os
 
-# Creating Output folder.
-if (os.path.exists("Output")):
-    delete1 = str('rm -r Output')
-    delete2 = str('rm logs.txt')
-    os.system(delete1)
-    os.system(delete2)
-    os.makedirs("Output")
-else:
-    os.makedirs("Output")
+count = 0
+try:
+    # Creating Output folder.
+    if (os.path.exists("Output")):
+        delete1 = str('rm -r Output')
+        delete2 = str('rm logs.txt')
+        os.system(delete1)
+        os.system(delete2)
+        os.makedirs("Output")
+    else:
+        os.makedirs("Output")
 
-# Making a log file.
-logs = open('logs.txt','w')
-logs.write("Following links threw errors. Have to do manually -->\n\n")
+    # Making a log file.
+    logs = open('logs.txt','w')
+    logs.write("Following links threw errors. Have to do manually -->\n\n")
 
-# Printing unecessary information for 'wanna be cool' people.
-banner = Figlet(font='slant')
-print (banner.renderText('Amazon Scraper'))
-print (">>>>>> Amazon Review Scraper <<<<<<")
-print (">>>>>> Author : ConanKapoor  <<<<<<\n")
+    # Printing unecessary information for 'wanna be cool' people.
+    banner = Figlet(font='slant')
+    print (banner.renderText('Amazon Scraper'))
+    print (">>>>>> Amazon Review Scraper <<<<<<")
+    print (">>>>>> Author : ConanKapoor  <<<<<<\n")
 
-# Taking url as input.
-url = input(">>> Please Enter the REVIEW URL for the product : \n    ")
-tempurl= url.split("/")
-ProductName = tempurl[3]
-tempurl = tempurl[0:6]
-tempurl = '/'.join(tempurl)
+    # Taking url as input.
+    url = input(">>> Please Enter the REVIEW URL for the product : \n    ")
+    tempurl= url.split("/")
+    ProductName = tempurl[3]
+    tempurl = tempurl[0:6]
+    tempurl = '/'.join(tempurl)
 
-# Scraping needed data.
-modifiedurl = tempurl + "/ref=cm_cr_arp_d_paging_btm_" + "1" +"?showViewpoints=1&pageNumber=" + "1"
-time.sleep(1)
-print ("\n\n>>> Modified url for getting Pagination Data - \n    %s"%(modifiedurl))
-request = urllib.request.Request(modifiedurl)
-response = urllib.request.urlopen(request)
+    # Scraping needed data.
+    modifiedurl = tempurl + "/ref=cm_cr_arp_d_paging_btm_" + "1" +"?showViewpoints=1&pageNumber=" + "1"
+    time.sleep(1)
+    print ("\n\n>>> Modified url for getting Pagination Data - \n    %s"%(modifiedurl))
+    request = urllib.request.Request(modifiedurl)
+    response = urllib.request.urlopen(request)
 
-# Using BeautifulSoup to parse html object response.
-soup = BeautifulSoup(response.read(),"lxml")
+    # Using BeautifulSoup to parse html object response.
+    soup = BeautifulSoup(response.read(),"lxml")
 
-# Using BeautifulSoup to find number of Paginations.
-Pagination = soup.find("ul", {"class": "a-pagination"})
-Pages = Pagination.find_all("li",{"class": "page-button"})
-LastPage = int(Pages[len(Pages)-1].text.replace(",",""))
+    # Using BeautifulSoup to find number of Paginations.
+    Pagination = soup.find("ul", {"class": "a-pagination"})
+    if Pagination is not None:
+        Pages = Pagination.find_all("li",{"class": "page-button"})
+        LastPage = int(Pages[len(Pages)-1].text.replace(",",""))
+    else:
+        LastPage = 1
 
-############ Now LastPage contains the number of links to scrape ##############
-###################### Main Scraping Logic Starts Below #######################
-print ("\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-print ("\t\t>>>>>> Review Scraping Starts  <<<<<<")
-print ("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
-print (">>> Product Name - %s "%(ProductName))
-print(">>> No of Review Pages - %s\n"%(LastPage))
+    ############ Now LastPage contains the number of links to scrape ##############
+    ###################### Main Scraping Logic Starts Below #######################
+    print ("\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    print ("\t\t>>>>>> Review Scraping Starts  <<<<<<")
+    print ("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
+    print (">>> Product Name - %s "%(ProductName))
+    print(">>> No of Review Pages - %s\n"%(LastPage))
 
-# Initiating XLSX file
-workbook = xlsxwriter.Workbook('Output/Reviews.xlsx',{'tmpdir': 'Output/TempReviews.xlsx'})
-worksheet = workbook.add_worksheet()
+    # Initiating XLSX file
+    workbook = xlsxwriter.Workbook('Output/Reviews.xlsx')
+    worksheet = workbook.add_worksheet()
 
-bold = workbook.add_format({'bold': True})
+    bold = workbook.add_format({'bold': True})
 
-worksheet.write('A1', 'Author', bold)
-worksheet.write('B1', 'Rating', bold)
-worksheet.write('C1', 'Date', bold)
-worksheet.write('D1', 'Attribute Available', bold)
-worksheet.write('E1', 'Attribute', bold)
-worksheet.write('F1', 'Verified Purchase', bold)
-worksheet.write('G1', 'Image Present', bold)
-worksheet.write('H1', 'Heading', bold)
-worksheet.write('I1', 'Description', bold)
-worksheet.write('J1', 'Number of Comments', bold)
-worksheet.write('K1', 'Upvotes', bold)
+    worksheet.write('A1', 'Author', bold)
+    worksheet.write('B1', 'Rating', bold)
+    worksheet.write('C1', 'Date', bold)
+    worksheet.write('D1', 'Attribute Available', bold)
+    worksheet.write('E1', 'Attribute', bold)
+    worksheet.write('F1', 'Verified Purchase', bold)
+    worksheet.write('G1', 'Image Present', bold)
+    worksheet.write('H1', 'Top 10 Reviewer', bold)
+    worksheet.write('I1', 'Heading', bold)
+    worksheet.write('J1', 'Description', bold)
+    worksheet.write('K1', 'Number of Comments', bold)
+    worksheet.write('L1', 'Upvotes', bold)
 
-count = 1
-for linkno in range(1,LastPage+1):
-    try:
-        # Making custom URL.
-        print("\n>>> Scraping Review Page - %s"%(linkno))
-        newurl = tempurl + "/ref=cm_cr_arp_d_paging_btm_" + str(linkno) +"?showViewpoints=1&pageNumber=" + str(linkno)
-        print("    Link - %s"%(newurl))
+    rows = 1
+    for linkno in range(1,LastPage+1):
+        try:
+            # Making custom URL.
+            print("\n>>> Scraping Review Page - %s"%(linkno))
+            newurl = tempurl + "/ref=cm_cr_arp_d_paging_btm_" + str(linkno) +"?showViewpoints=1&pageNumber=" + str(linkno)
+            print("    Link - %s"%(newurl))
 
-        # Collecting html content.
-        request = urllib.request.Request(newurl)
-        response = urllib.request.urlopen(request)
+            # Collecting html content.
+            request = urllib.request.Request(newurl)
+            response = urllib.request.urlopen(request)
 
-        # Using BeautifulSoup to parse html object response.
-        soup = BeautifulSoup(response.read(),"lxml")
+            # Using BeautifulSoup to parse html object response.
+            soup = BeautifulSoup(response.read(),"lxml")
 
-        # Collecting Reviews.
-        ReviewList = soup.find("div",{"id": "cm_cr-review_list"})
-        Reviews = soup.find_all("div",{"class":"a-section review"})
+            # Collecting Reviews.
+            ReviewList = soup.find("div",{"id": "cm_cr-review_list"})
+            Reviews = soup.find_all("div",{"class":"a-section review"})
 
-        # Scraping different features from all reviews.
-        rows = 1
-        for review in Reviews:
-            print("\n\n    >>> Scraping Review number - %s"%(count))
-            count=count+1
+            # Scraping different features from all reviews.
+            for review in Reviews:
+                count=count+1
+                print("    >>> Scraping Review number - %s"%(count))
 
-            # Scraping Author.
-            AuthorHTML = review.find("a",{"data-hook":"review-author"})
-            Author = AuthorHTML.text
+                # Scraping Author.
+                AuthorHTML = review.find("a",{"data-hook":"review-author"})
+                Author = AuthorHTML.text
 
-            # Scraping Rating.
-            RatingHTML = review.find("span",{"class": "a-icon-alt"})
-            Rating = int(RatingHTML.text[0])
+                # Scraping Rating.
+                RatingHTML = review.find("span",{"class": "a-icon-alt"})
+                Rating = int(RatingHTML.text[0])
 
-            # Scraping Date.
-            DateHTML = review.find("span",{"data-hook":"review-date"})
-            Date = DateHTML.text[3:]
+                # Scraping Date.
+                DateHTML = review.find("span",{"data-hook":"review-date"})
+                Date = DateHTML.text[3:]
 
-            # Scraping Attribute (if there)
-            AttributeHTML = review.find("a",{"data-hook":"format-strip"})
-            Attribute = AttributeHTML.text
+                # Scraping Attribute (if there)
+                AttributeHTML = review.find("a",{"data-hook":"format-strip"})
+                Attribute = AttributeHTML.text
 
-            # Is purchase verified?
-            VerifiedHTML = review.find("span",{"data-hook":"avp-badge"})
-            Verified = VerifiedHTML.text
+                # Is purchase verified?
+                VerifiedHTML = review.find("span",{"data-hook":"avp-badge"})
 
-            # Review Heading.
-            HeadingHTML = review.find("a",{"data-hook":"review-title"})
-            Heading = HeadingHTML.text
+                # Review Heading.
+                HeadingHTML = review.find("a",{"data-hook":"review-title"})
+                Heading = HeadingHTML.text
 
-            # Is image present in review?
-            ImageHTML = review.find("div",{"class":"review-image-tile-section"})
+                # Is image present in review?
+                ImageHTML = review.find("div",{"class":"review-image-tile-section"})
 
-            # Review Description
-            DescriptionHTML = review.find("span",{"data-hook":"review-body"})
-            Description = DescriptionHTML.text
+                # Top 10 reviewer?
+                TopHTML = review.find("span",{"class":"c7yTopDownDashedStrike"})
 
-            # Number of comments
-            CommentsHTML = review.find("span",{"class":"review-comment-total"})
-            Comments = CommentsHTML.text
+                # Review Description
+                DescriptionHTML = review.find("span",{"data-hook":"review-body"})
+                Description = DescriptionHTML.text
 
-            # Number of upvotes
-            UpvotesHTML = review.find("span",{"class":"review-votes"})
+                # Number of comments
+                CommentsHTML = review.find("span",{"class":"review-comment-total"})
+                Comments = int(CommentsHTML.text)
 
-            # Printing Data into Terminal
-            print("\n        Author : %s"%(Author))
-            print("        Rating : %s"%(Rating))
-            print("        Date   : %s\n"%(Date))
+                # Number of upvotes
+                UpvotesHTML = review.find("span",{"class":"review-votes"})
 
-            if Attribute is not None:
-                print("        Attribute Available : Yes")
-                print("        Attribute   : %s\n"%(Attribute))
-            else:
-                print("        Attribute Available : No\n")
+                # Uncomment below to Print Data into Terminal
+                '''
+                print("\n        Author : %s"%(Author))
+                print("        Rating : %s"%(Rating))
+                print("        Date   : %s\n"%(Date))
 
-            if Verified == "Verified Purchase":
-                print("        Verified Purchase : Yes\n")
-            else:
-                print("        Verified Purchase : No\n")
+                if Attribute is not None:
+                    print("        Attribute Available : Yes")
+                    print("        Attribute   : %s\n"%(Attribute))
+                else:
+                    print("        Attribute Available : No\n")
 
-            if ImageHTML is not None:
-                print("        Image Present : Yes\n")
-            else:
-                print("        Image Present : No\n")
+                if VerifiedHTML is not None:
+                    if VerifiedHTML.text == "Verified Purchase" or TopHTML is not None:
+                        print("        Verified Purchase : Yes\n")
+                    else:
+                        print("        Verified Purchase : No\n")
+                else:
+                    print("        Verified Purchase : No\n")
 
-            print("        Heading  : %s"%(Heading))
-            print("        Description  : %s\n"%(Description))
-            print("        Number of Comments : %s"%(Comments))
+                if ImageHTML is not None:
+                    print("        Image Present : Yes\n")
+                else:
+                    print("        Image Present : No\n")
 
-            if UpvotesHTML is None:
-                print("        Upvotes : 0")
-            elif UpvotesHTML.text.split(" ")[6] == "One":
-                print("        Upvotes : 1")
-            else:
-                Upvotes = UpvotesHTML.text
-                Upvotes = Upvotes.split(" ")
-                print("        Upvotes : %s"%(Upvotes[6]))
+                if TopHTML is not None:
+                    print("        Top 10 Reviewer : Yes\n")
+                else:
+                    print("        Top 10 Reviewer : No\n")
 
-        # Saving Data into XLSX
-        worksheet.write(rows, 1, Author)
-        worksheet.write(rows, 2, Rating)
-        worksheet.write(rows, 3, Date)
+                print("        Heading  : %s"%(Heading))
+                print("        Description  : %s\n"%(Description))
+                print("        Number of Comments : %s"%(Comments))
 
-        if Attribute is not None:
-            worksheet.write(rows, 4, "Yes")
-            worksheet.write(rows, 5, Attribute)
-        else:
-            worksheet.write(rows, 4, "No")
-            worksheet.write(rows, 5, None)
+                if UpvotesHTML is None:
+                    print("        Upvotes : 0")
+                elif UpvotesHTML.text.split(" ")[6] == "One":
+                    print("        Upvotes : 1")
+                else:
+                    Upvotes = UpvotesHTML.text
+                    Upvotes = Upvotes.split(" ")
+                    print("        Upvotes : %s"%(Upvotes[6]))
+            '''
 
-        if Verified == "Verified Purchase":
-            worksheet.write(rows, 6, "Yes")
-        else:
-            worksheet.write(rows, 6, "No")
+                # Saving Data into XLSX
+                worksheet.write(rows, 0, Author)
+                worksheet.write(rows, 1, Rating)
+                worksheet.write(rows, 2, Date)
 
-        if ImageHTML is not None:
-            worksheet.write(rows, 7, "Yes")
-        else:
-            worksheet.write(rows, 7, "No")
+                if Attribute is not None:
+                    worksheet.write(rows, 3, "Yes")
+                    worksheet.write(rows, 4, Attribute)
+                else:
+                    worksheet.write(rows, 3, "No")
+                    worksheet.write(rows, 4, None)
 
-        worksheet.write(rows, 8, Heading)
-        worksheet.write(rows, 9, Description)
-        worksheet.write(rows, 10, Comments)
+                if VerifiedHTML is not None:
+                    if VerifiedHTML.text == "Verified Purchase" or TopHTML is not None:
+                        worksheet.write(rows, 5, "Yes")
+                    else:
+                        worksheet.write(rows, 5, "No")
+                else:
+                    worksheet.write(rows, 5, "No")
 
-        if UpvotesHTML is None:
-            worksheet.write(rows, 11, 0)
-        elif UpvotesHTML.text.split(" ")[6] == "One":
-            worksheet.write(rows, 11, 1)
-        else:
-            Upvotes = UpvotesHTML.text
-            Upvotes = Upvotes.split(" ")
-            worksheet.write(rows, 11, Upvotes[6])
+                if ImageHTML is not None:
+                    worksheet.write(rows, 6, "Yes")
+                else:
+                    worksheet.write(rows, 6, "No")
 
-        rows = rows + 1
+                if TopHTML is not None:
+                    worksheet.write(rows, 7, "Yes")
+                else:
+                    worksheet.write(rows, 7, "No")
 
-    except Exception:
-        print("\t>!>!> Exceptioncaught - skipping to next link.(Check logs.txt)\n")
-        newurl = tempurl + "/ref=cm_cr_arp_d_paging_btm_" + str(linkno) +"?showViewpoints=1&pageNumber=" + str(linkno)
-        logs.write("Exception error - %s \n\n" %(newurl))
-        pass
+                worksheet.write(rows, 8, Heading)
+                worksheet.write(rows, 9, Description)
+                worksheet.write(rows, 10, Comments)
 
-# Closing the workbook and logs
-workbook.close()
-logs.close()
+                if UpvotesHTML is None:
+                    worksheet.write(rows, 11, 0)
+                elif UpvotesHTML.text.split(" ")[6] == "One":
+                    worksheet.write(rows, 11, 1)
+                else:
+                    Upvotes = UpvotesHTML.text
+                    Upvotes = Upvotes.split(" ")
+                    worksheet.write(rows, 11, int(Upvotes[6]))
+
+                rows = rows + 1
+
+        except Exception:
+            print("\t>!>!> Exceptioncaught - skipping to next link.(Check logs.txt)\n")
+            newurl = tempurl + "/ref=cm_cr_arp_d_paging_btm_" + str(linkno) +"?showViewpoints=1&pageNumber=" + str(linkno)
+            logs.write("Exception error - %s \n\n" %(newurl))
+            pass
+
+    # Closing the workbook and logs
+    print("\n>>> Total number of reviews scraped - %s\n"%(count))
+    logs.write("End of logs - Complete Scraping done")
+    workbook.close()
+    logs.close()
+
+except KeyboardInterrupt:
+    print("\n\nInterrupt received! Exiting cleanly...")
+    print(">>> Total number of reviews scraped - %s\n"%(count))
+    logs.write("End of logs - Abruptly Ended")
+    workbook.close()
+    logs.close()
+    sys.exit()
